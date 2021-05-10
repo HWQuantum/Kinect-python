1e881bfac1abefbd52a2cd22d37771d3a979f378
Alice Ruget <alice.ruget@free.fr>
2021-05-10 11:10:34 +0100

    Crop

--- kinect_python_2.py
+++ kinect_python_2.py
@@ -6,14 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tools_kinect 
 import time
+import cv2
 import scipy.io as sio
 d, calibration = tools_kinect.initialise_device_with_calibration()
 im = tools_kinect.get_image(d)
 
 
 
-N = 100
-
+N = 300
+print('TAKE DATA')
 depth_img_full = np.empty((576,640,N)) #np.empty((474,430,N))
 color_img_full = np.empty((720,1280,4,N))
 transformed_depth_img_full =  np.empty((720,1280,N))
@@ -34,16 +35,38 @@ for index_image in range(N):
 
 time_end = time.time()
 print( "processing time="+str(time_end-time_begin))
+
+color_img_full = color_img_full[50:, 325:925,:3,:]
+transformed_depth_img_full = transformed_depth_img_full[50:, 325:925,:]
+depth_img_full = depth_img_full[51:525, 105:535,:]
+
+# # # # # Processing
+print('PROCESSING')
+color_img_resized = np.empty((223,200,3,N))
+transformed_depth_img_resized =  np.empty((223,200,N))
+for index_image in range(N):
+    color_img_resized[:,:,:,index_image] = cv2.resize(color_img_full[:,:,:,index_image], fx=3,fy=3,  dsize=(200, 223), interpolation= cv2.INTER_NEAREST)
+    transformed_depth_img_resized[:,:,index_image] = cv2.resize(transformed_depth_img_full[:,:,index_image], fx=3,fy=3,  dsize=(200, 223), interpolation= cv2.INTER_NEAREST)
+    
+
+    
+
+
 fig, axs = plt.subplots(1, 2)
-axs[0].imshow(colour)
-axs[1].imshow(depth)
+axs[0].imshow(transformed_depth_img_resized[:,:,0])
+axs[1].imshow(color_img_resized[:,:,0])
 plt.show()
 
-name = 'first_test.mat'
+print('SAVE')
+name = '3D_proof_of_concept_'
 dictio = {}
-dictio['transformed_depth_img_full'] = transformed_depth_img_full
+dictio['transformed_depth_img_full'] = transformed_depth_img_resized
+sio.savemat(name+'depth_transformed.mat', dictio)
+dictio = {}
 dictio['depth'] = depth_img_full
-dictio['colour'] = color_img_full
-sio.savemat(name, dictio)
+sio.savemat(name+'depth.mat', dictio)
+dictio = {}
+dictio['colour'] = color_img_resized
+sio.savemat(name+'color.mat', dictio)
 tools_kinect.k4a_device_close(d)
 
